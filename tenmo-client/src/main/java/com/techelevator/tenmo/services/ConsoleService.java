80c1812ae4b690c527cd968855afada0c45976e0
Gaby Frome <gabrieladfrome@outlook.com>
2022-08-30 13:07:46 -0500

    Gaby changed Main menu

--- tenmo-client/src/main/java/com/techelevator/tenmo/services/ConsoleService.java
+++ tenmo-client/src/main/java/com/techelevator/tenmo/services/ConsoleService.java
@@ -45,7 +45,7 @@ public class ConsoleService {
         System.out.println("3: View your pending requests");
         System.out.println("4: Send TE bucks");
         System.out.println("5: Request TE bucks");
-        System.out.println("0: Exit");
+        System.out.println("0: Return to login");
         System.out.println();
     }
 
